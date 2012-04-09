51c8f23f04797f3dae96481b54d395976d9661ad
Dave King <tildedave@gmail.com>
2012-04-09 08:44:11 -0400

    Basic when matcher.

--- spec/mock_spec.js
+++ spec/mock_spec.js
@@ -1,6 +1,6 @@
 var mock = require('../src/mock.js').mock;
 
-describe('Mock', function () {
+describe('mock', function () {
 
   var Clazz;
   
@@ -32,5 +32,33 @@ describe('Mock', function () {
     var mockInstance = mock(Clazz);
 
     expect(invoked).toBe(false);
+  });
+});
+
+var when = require('../src/mock.js').when;
+
+describe('when', function () {
+
+  var Clazz;
+  
+  beforeEach(function () {
+    Clazz = function () { };
+    Clazz.prototype.foo = function () { };
+  });
+  
+  it('defines behavior based on a matcher', function () {
+    var instance = mock(Clazz);
+    when(instance.foo).isCalledWith(5).thenReturn("zebra");
+
+    expect(instance.foo(5)).toBe("zebra");
+  });
+
+  it('installs multiple behaviors', function () {
+    var instance = mock(Clazz);
+    when(instance.foo).isCalledWith(3).thenReturn("dog");
+    when(instance.foo).isCalledWith(4).thenReturn("cat");
+
+    expect(instance.foo(4)).toBe("cat");
+    expect(instance.foo(3)).toBe("dog");
   });
 });
--- src/mock.js
+++ src/mock.js
@@ -9,7 +9,46 @@ var mock = function (clazz) {
   return instance;
 };
 
+var whenMatcher = function (args, value) {
+  return {
+    matches: function (matchArgs) {
+      return args === matchArgs;
+    },
+    returnValue: value
+  };
+};
+
+var when = function (spy) {
+  return {
+    isCalledWith: function (args) {
+      return {
+        thenReturn: function (value) {
+          if (!spy.jasmine_mock__installedMatchers) {
+            spy.jasmine_mock__installedMatchers = [];
+            spy.andCallFake(function (spyArgs) {
+              var matchers = spy.jasmine_mock__installedMatchers;
+              for (var i = 0, l = matchers.length; i < l; ++i) {
+                var matcher = matchers[i];
+                if (matcher.matches(spyArgs)) {
+                  return matcher.returnValue;
+                }
+              }
+
+              return undefined;
+            });
+          }
+
+          spy.jasmine_mock__installedMatchers.push(
+            whenMatcher(args, value)
+          );
+        }
+      };
+    }
+  };
+};
+
 module.exports = {
-  mock: mock
+  mock: mock,
+  when: when
 };
 
